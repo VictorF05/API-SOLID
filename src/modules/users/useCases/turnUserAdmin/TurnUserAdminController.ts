10a36a24d14704b7ff3df906cac119fc269bafef
Víctor Franco <victorfranco5@outlook.com>
2022-09-27 20:44:48 -0300

    challenge started

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -3,11 +3,15 @@ import { Request, Response } from "express";
 import { TurnUserAdminUseCase } from "./TurnUserAdminUseCase";
 
 class TurnUserAdminController {
-  constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
+    constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
-  }
+    handle(request: Request, response: Response): Response {
+        const { user_id } = request.params;
+
+        const userAdmin = this.turnUserAdminUseCase.execute({ user_id });
+
+        return response.status(200).json(userAdmin);
+    }
 }
 
 export { TurnUserAdminController };
